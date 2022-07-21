8c73602838410dae9e015dc4b0efc312c5f37b7f
jazzypants1989 <jessepence@gmail.com>
2022-07-20 22:57:42 -0700

    changing thangs

--- server.js
+++ server.js
@@ -1,5 +1,20 @@
+// 1.) Initialize npm and insall dependencies
+// -Express, dotenv, cors, mongodb, ejs, nodemon(--save-dev)
+// 2.) Require dependencies
 const express = require ('express')
 const app = express()
-const mongoClient = require ('mongodb').MongoClient
-const dotenv = require ('dotenv').config()
+const MongoClient = require ('mongodb').MongoClient
 const cors = require ('cors')
+require ('dotenv').config()
+// 3.) Declare variables
+let db,
+dbConnectionString = proccess.env.DB_STRING,
+dbName = 'sample_mflix',
+collections
+
+MongoClient.connect(dbConnectionString)
+    .then(client => {
+        console.log('Connected to Database')
+        db = client.db(dbName)
+        collection = db.collection('movies')
+    }
